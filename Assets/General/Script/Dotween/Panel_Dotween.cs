dd2ccaefd6705f8c1459a2d611722666fa390fc6
Unknown User <asd@ritimus.com>
2023-05-09 19:51:09 +0300

    Dotween menu and button aNİM

---
+++ Assets/General/Script/Dotween/Panel_Dotween.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Panel_Dotween : MonoBehaviour
+{
+    public float fadeTime = 1f;
+    public CanvasGroup canvasGroup;
+   
+}
--- Assets/General/Script/OpenScenes.cs
+++ Assets/General/Script/OpenScenes.cs
@@ -2,24 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class OpenScenes : MonoBehaviour
 {
     public GameObject touchPanel;
     public GameObject Panel;
+    public CanvasGroup canvasGroup;
+    RectTransform rectTransformTouchPanel;
+    public float fadeTime;
+    public List<GameObject> items = new List<GameObject>();
 
     public void TouchOpen(GameObject panel)
     {
         touchPanel = panel;
-        panel.SetActive(true);      
-    }
 
+        rectTransformTouchPanel = touchPanel.GetComponent<RectTransform>();
+        rectTransformTouchPanel.transform.localPosition = new Vector3(0, -400f, 0f);
+        rectTransformTouchPanel.DOAnchorPos(new Vector2(0f, 0f), fadeTime, false).SetEase(Ease.OutElastic);
+
+        panel.SetActive(true);
+    }
+    private void Start()
+    {
+
+        StartCoroutine(ItemAnimation());
+    }
     public void ClosePanel(GameObject panel)
     {
-        
-            panel.SetActive(false);
-        
+        rectTransformTouchPanel = touchPanel.GetComponent<RectTransform>();
+        rectTransformTouchPanel.transform.localPosition = new Vector3(0, 0f, 0f);
+        rectTransformTouchPanel.DOAnchorPos(new Vector2(0f, -4000f), fadeTime, false).SetEase(Ease.InOutQuint);
+
+
+
         touchPanel = null;
+    }
+
+
+    IEnumerator ItemAnimation()
+    {
+        foreach (var item in items)
+        {
+            item.transform.localScale = Vector3.zero;
+        }
+        foreach (var item in items)
+        {
+            item.transform.DOScale(1f, 1f).SetEase(Ease.OutBounce);
+            yield return new WaitForSeconds(0.25f);
+        }
     }
 
     public void OpenScene(int sceneInt)
