1ffb3e773a2ba96cf1017eb25065b6822f8ee5d5
Unknown User <asd@ritimus.com>
2023-05-15 16:52:10 +0300

    FireBase_RemoteConfig

---
+++ Assets/Games/Track - 03/Script/BallController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BallController : MonoBehaviour
+{
+    public GameObject m_PlayerObj;
+    public Transform[] positionPoint;
+    [Range(0,1)]
+    public float value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(iTween.PathLength(positionPoint));
+    }
+    float tempTime;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (value < 2)
+        {
+            value += Time.deltaTime / 10;
+        }
+        iTween.PutOnPath(m_PlayerObj, positionPoint, value);
+    }
+    private void OnDrawGizmos()
+    {
+        
+        iTween.DrawPath(positionPoint,Color.green);
+    }
+}
+
---
+++ Assets/General/Script/RemoteConfigs/MyFireBaseRemoteConfigs.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Firebase.Extensions;
+
+public class MyFireBaseRemoteConfigs : MonoBehaviour
+{
+    Firebase.DependencyStatus dependencyStatus = Firebase.DependencyStatus.UnavailableOther;
+    // Use this for initialization
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                InitializeFirebase();
+            }
+            else
+            {
+                Debug.LogError(
+                    "Could not resolve all Firebase dependencies: " + dependencyStatus);
+            }
+        });
+    }
+
+    void InitializeFirebase()
+    {
+        System.Collections.Generic.Dictionary<string, object> defaults =
+            new System.Collections.Generic.Dictionary<string, object>();
+
+        // These are the values that are used if we haven't fetched data from the
+        // server
+        // yet, or if we ask for values that the server doesn't have:
+        defaults.Add("config_test_string", "default local string");
+        defaults.Add("config_test_int", 1);
+        defaults.Add("config_test_float", 1.0);
+        defaults.Add("config_test_bool", false);
+
+        Firebase.RemoteConfig.FirebaseRemoteConfig.DefaultInstance.SetDefaultsAsync(defaults);
+        Debug.Log("Remote config ready!");
+    }
+    public void FetchFireBase()
+    {
+        FetchDataAsync();
+    }
+    public void ShowData()
+    {
+        //   DebugLog("config_test_string: " +
+        //        Firebase.RemoteConfig.FirebaseRemoteConfig.GetValue("config_test_string").StringValue);
+
+        //   DebugLog("config_test_float: " +
+        //            Firebase.RemoteConfig.FirebaseRemoteConfig.GetValue("config_test_float").DoubleValue);
+
+        var go = Firebase.RemoteConfig.FirebaseRemoteConfig.DefaultInstance.GetValue("Tests").BooleanValue;
+    }
+
+    // Start a fetch request.
+    public Task FetchDataAsync()
+    {
+        Debug.Log("Fetching data...");
+        // FetchAsync only fetches new data if the current data is older than the provided
+        // timespan.  Otherwise it assumes the data is "recent enough", and does nothing.
+        // By default the timespan is 12 hours, and for production apps, this is a good
+        // number.  For this example though, it's set to a timespan of zero, so that
+        // changes in the console will always show up immediately.
+        System.Threading.Tasks.Task fetchTask = Firebase.RemoteConfig.FirebaseRemoteConfig.DefaultInstance.FetchAsync(
+            TimeSpan.Zero);
+        return fetchTask.ContinueWith(FetchComplete);
+    }
+
+    void FetchComplete(Task fetchTask)
+    {
+        if (!fetchTask.IsCompleted)
+        {
+            Debug.LogError("Retrieval hasn't finished.");
+            return;
+        }
+
+        var remoteConfig = Firebase.RemoteConfig.FirebaseRemoteConfig.DefaultInstance;
+        var info = remoteConfig.Info;
+       
+
+        // Fetch successful. Parameter values must be activated to use.
+        remoteConfig.ActivateAsync()
+          .ContinueWithOnMainThread(
+            task =>
+            {
+                Debug.Log($"Remote data loaded and ready for use. Last fetch time {info.FetchTime}.");
+            });
+    }
+}
